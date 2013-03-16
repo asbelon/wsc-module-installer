e61a7c3aac12db236656c6d252a469f18a5ffaa7
Aleksandr Belov <asbelon@gmail.com>
2013-03-16 05:26:07 +0400

    1.0.0.4

--- data_presets.php
+++ data_presets.php
@@ -1,10 +1,20 @@
 <?php
 
-$presets['division'] = array(
-	array(
-		'name' => '',
-		'value' => '',
-		'type' => '',
-		'required' => ''
-	),
-);
+class installerPreset {
+
+	static public $DIVISION = array(
+		array(
+			'name' => 'xName',
+			'value' => '',
+			'type' => 'string',
+			'required' => true
+		),
+		array(
+			'name' => 'xKey',
+			'value' => '',
+			'type' => 'string',
+			'required' => false
+		),
+	);
+
+}
